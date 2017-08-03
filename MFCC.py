7e4104898995d8182fe89b8e7fb91ae5176b1d16
Henry Bosch <henry@dhcp-18-111-4-248.dyn.MIT.EDU>
2017-08-03 10:50:03 -0400

    Made into importable module.

--- MFCC.py
+++ MFCC.py
@@ -7,6 +7,8 @@ from mygrad.nnet.losses import multiclass_hinge
 from microphone import record_audio
 import math
 
+__all__ = ["file_to_array", "mic_to_numpy_array", "fft", "get_freqs", "mel_scale", "dct", "compute_accuracy", "sgd", "dense_NN"]
+
 fs = 44100
 
 # Need 250ms intervals. Since, 44100Hz = 44100 samples/s, (44100/4) cycles/(250ms). Need 44100/4 = 11025 samples.
@@ -53,11 +55,11 @@ def fft(song_arr):
 
 	:param
 		song_arr[np.array]:
-			This is the array that represents the song. 
+			This is the array that represents the song.
 
 	:return:
 		f[np.array]:
-			This is the array of coefficients of the fft. 
+			This is the array of coefficients of the fft.
 
 	"""
 
@@ -79,27 +81,27 @@ def mel_scale(fft_arr):
 
 	:return:
 		mels[np.array]:
-			This is an array that transformed the fft from Hertz into Mels. 
+			This is an array that transformed the fft from Hertz into Mels.
 
 	"""
 	return 2595*np.log10(1 + fft_arr/700)
 
 def dct(mel_arr):
 	"""
-	This does the dct of the 
+	This does the dct of the
 	"""
 	return scipy.fftpack.dct(mel_arr, n=12)
 
 def compute_accuracy(model_out, labels):
     """ Computes the mean accuracy, given predictions and true-labels.
-        
+
         Parameters
         ----------
         model_out : numpy.ndarray, shape=(N, K)
             The predicted class-scores
         labels : numpy.ndarray, shape=(N, K)
             The one-hot encoded labels for the data.
-        
+
         Returns
         -------
         float
@@ -108,12 +110,12 @@ def compute_accuracy(model_out, labels):
 
 def sgd(param, rate):
     """ Performs a gradient-descent update on the parameter.
-    
+
         Parameters
         ----------
         param : mygrad.Tensor
             The parameter to be updated.
-        
+
         rate : float
             The step size used in the update"""
     param.data -= rate*param.grad
@@ -125,18 +127,17 @@ def dense_NN(W, b, xtrain):
 
 	:Returns:
 		(W, b)(tuple of training parameters):
-			The W and b are the same as inputted but changed. 
+			The W and b are the same as inputted but changed.
 	"""
 	for i in range(1000):
 	    o = dense(xtrain, W) + b
-	    
+
 	    loss = multiclass_hinge(o, y)
-	    
+
 	    loss.backward()
-	    
+
 	    sgd(W, 0.1)
 	    sgd(b, 0.1)
-	    
+
 	    loss.null_gradients()
 	return (W, b)
-	
