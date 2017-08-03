7fbaa329de1984e82752307ea4d7d9b1261448ef
Manu Varma <varmam@bxscience.edu>
2017-08-03 10:01:12 -0400

    Added dense

--- MFCC.py
+++ MFCC.py
@@ -1,6 +1,7 @@
 import numpy
 import scipy.fftpack
 import librosa
+import mygrad
 from microphone import record_audio
 import math
 
@@ -60,6 +61,12 @@ def fft(song_arr):
 
 	return np.fft.rfft(song_arr)
 
+def get_freqs(song_arr):
+    dft = np.fft.rfft(song_arr)
+    times = np.arange(len(song_arr))
+    times = times / 44100
+    return np.arange(len(dft)) / times[-1]
+
 def mel_scale(fft_arr):
 	"""
 	This scales the frequencies from hertz into mels.
@@ -76,4 +83,57 @@ def mel_scale(fft_arr):
 	return 2595*np.log10(1 + fft_arr/700)
 
 def dct(mel_arr):
+	"""
+	This does the dct of the 
+	"""
 	return scipy.fftpack.dct(mel_arr, n=12)
+
+def compute_accuracy(model_out, labels):
+    """ Computes the mean accuracy, given predictions and true-labels.
+        
+        Parameters
+        ----------
+        model_out : numpy.ndarray, shape=(N, K)
+            The predicted class-scores
+        labels : numpy.ndarray, shape=(N, K)
+            The one-hot encoded labels for the data.
+        
+        Returns
+        -------
+        float
+            The mean classification accuracy of the N samples."""
+    return np.mean(np.argmax(model_out, axis=1) == np.argmax(labels, axis=1))
+
+def sgd(param, rate):
+    """ Performs a gradient-descent update on the parameter.
+    
+        Parameters
+        ----------
+        param : mygrad.Tensor
+            The parameter to be updated.
+        
+        rate : float
+            The step size used in the update"""
+    param.data -= rate*param.grad
+    return None
+
+def dense_NN(W, b, xtrain):
+	"""
+	Does a dense Neural Network on xtrain and updates W and b.
+
+	:Returns:
+		(W, b)(tuple of training parameters):
+			The W and b are the same as inputted but changed. 
+	"""
+	for i in range(1000):
+	    o = dense(xtrain, W) + b
+	    
+	    loss = multiclass_hinge(o, y)
+	    
+	    loss.backward()
+	    
+	    sgd(W, 0.1)
+	    sgd(b, 0.1)
+	    
+	    loss.null_gradients()
+	return (W, b)
