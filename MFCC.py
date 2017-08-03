1ef20eaa67ca61a9c1e1972d8c2c420fe16c1d68
Manu Varma <varmam@bxscience.edu>
2017-08-03 16:48:03 -0400

    Added the 1 layer Dense NN

--- MFCC.py
+++ MFCC.py
@@ -5,7 +5,8 @@ import mygrad
 from mygrad.nnet.layers import dense
 from mygrad.nnet.losses import multiclass_hinge
 from microphone import record_audio
-import math
+import pickle 
+import os
 
 __all__ = ["file_to_array", "mic_to_numpy_array", "fft", "get_freqs", "mel_scale", "dct", "compute_accuracy", "sgd", "dense_NN"]
 
@@ -88,7 +89,7 @@ def mel_scale(fft_arr):
 
 def dct(mel_arr):
 	"""
-	This does the dct of the
+	This does the dct of the mel_arr.
 	"""
 	return scipy.fftpack.dct(mel_arr, n=12)
 
@@ -146,3 +147,42 @@ def dense_NN(W, b, xtrain):
 
 	    loss.null_gradients()
 	return (W, b)
+
+original_path = r"C:\Users\manusree\PycharmProjects\Alexa Skills\Genre-Identification\SongTraining"
+changed_path = r"C:\Users\manusree\PycharmProjects\Alexa Skills\Genre-Identification\SongTraining"
+a = os.listdir(r"C:\Users\manusree\PycharmProjects\Alexa Skills\Genre-Identification\SongTraining")
+ytrain = []
+MFCCs = []
+interval_time = 10
+for genre in a:
+    changed_path1 = original_path + "\\" + genre
+    changed_path2 = original_path + "\\" + genre
+    x = os.listdir(changed_path1)
+    for i in range(len(x)):
+        changed_path2 = changed_path1 + "\\" + x[i]
+        song_arr = file_to_array(changed_path2)
+        for k in range(0, int(len(song_arr)/fs)):
+            interval = song_arr[k*fs*interval_time:(k+1)*fs*interval_time]
+            if interval != np.array([]):
+                b = to_MFCC(interval)
+                MFCCs.append(b)
+                del b
+                del interval
+                if genre == "Classical":
+                    ytrain.append([1,0,0,0,0])
+                elif genre == "Jazz":
+                    ytrain.append([0,1,0,0,0])
+                elif genre == "Pop":
+                    ytrain.append([0,0,1,0,0])
+                elif genre == "Rap":
+                    ytrain.append([0,0,0,1,0])
+                elif genre == "Rock":
+                    ytrain.append([0,0,0,0,1])
+        del song_arr
+MFCCs = np.array(MFCCs)
+ytrain = np.array(ytrain)
+y = np.where(ytrain == 1)[1]
+a = Tensor(np.random.randn(12, 5))
+c = Tensor(np.zeros((5,), dtype=a.dtype))
+W, b, acc = dense_NN(a, c, MFCCs, ytrain)
+print(acc[-1])
