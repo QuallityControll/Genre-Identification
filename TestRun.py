46a451c17bc2778cdbc058d77bb34e719eef9e62
Manu Varma <varmam@bxscience.edu>
2017-08-04 15:29:51 -0400

    This stuff

--- TestRun.py
+++ TestRun.py
@@ -40,29 +40,42 @@ for i in range(len(song_paths)):
     del b
     del a
 
+a = np.append(MFCCs[:14],MFCCs[15:])
+a = a.reshape((26,12))
+a -= np.mean(a, axis=0)
+a = a/np.std(a, axis=0)
+b = np.append(ytrain[:14],ytrain[15:])
+b = b.reshape((26,5))
+
 D = 12
 K = 5
-
-W1 = Tensor(he_normal((D, 100)))
-b1 = Tensor(np.zeros((100,), dtype=W1.dtype))
-W2 = Tensor(he_normal((100, K)))
+N = 100
+W1 = Tensor(he_normal((D, N)))
+b1 = Tensor(np.zeros((N,), dtype=W1.dtype))
+W2 = Tensor(he_normal((N, K)))
 b2 = Tensor(np.zeros((K,), dtype=W2.dtype))
 params = [b1, W1, b2, W2]
 
-rate = 1.
+rate = 0.01
+reg = 0.1
 
 l = []
 acc = []
 for i in range(1000):
-    o1 = relu(dense(xtrain, W1) + b1)
+    o1 = relu(dense(a, W1) + b1)
     p_pred = softmax(dense(o1, W2) + b2)
-
-    loss = cross_entropy(p_pred=p_pred, p_true=ytrain)
+    #print(p_pred)
+    
+    reg_sum = Tensor(0)
+    for p in params:
+        reg_sum += reg*(p**2).sum()
+    
+    loss = cross_entropy(p_pred=p_pred, p_true=b) + reg_sum
     
     l.append(loss.data.item())
     loss.backward()
 
-    acc.append(compute_accuracy(p_pred.data, ytrain))
+    acc.append(compute_accuracy(p_pred.data, b))
     
     for param in params:
         sgd(param, rate)
